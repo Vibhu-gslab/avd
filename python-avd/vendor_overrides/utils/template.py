475f5c2fa40e917f9637cfcdd540c23cd2cd9d45
Claus Holbech <holbech@arista.com>
2023-04-04 20:11:38 +0200

    Remove more files with ansible references

--- python-avd/vendor_overrides/utils/template.py
+++ python-avd/vendor_overrides/utils/template.py
@@ -3,5 +3,5 @@ Dummy. Not needed for pyavd, but is imported in __init__.py
 """
 
 
-def get_templar():
+def template(*args) -> str:
     raise NotImplementedError("Jinja Templating is not implemented in pyavd")
