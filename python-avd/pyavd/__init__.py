b667b4f03ea262e118525f08f1f4d81c234bc54f
Vibhu Tripathi <vibhu.tripathi-ext@arista.com>
2023-05-10 13:37:17 +0530

    addressing review comments

--- python-avd/pyavd/__init__.py
+++ python-avd/pyavd/__init__.py
@@ -1,7 +1,8 @@
 from .eos_cli_config_gen import eos_cli_config_gen
 from .eos_designs_facts import eos_designs_facts
 from .eos_designs_structured_configs import eos_designs_structured_configs
-from .version import VERSION
+import importlib.metadata
+
 
 """ Library for running Arista Validated Designs (AVD) in Python
 """
@@ -11,6 +12,6 @@ NAME = "pyavd"
 __author__ = "Arista Networks"
 __copyright__ = "Copyright 2023 Arista Networks"
 __license__ = "Apache 2.0"
-__version__ = VERSION
+__version__ = importlib.metadata.version(NAME)
 
 __all__ = ["eos_cli_config_gen", "eos_designs_facts", "eos_designs_structured_configs"]
